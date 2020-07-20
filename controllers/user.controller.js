afdb61fda4ad13630047d2b1026e4f0f013e6f88
AnhBui <buitheanh1990@gmail.com>
2020-07-20 23:43:03 +0700

    update file

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,65 +1,75 @@
-const db = require('../db');
-const shortid = require('shortid');
-const date = require('date-and-time');
-const bcrypt = require('bcrypt');
+const db = require("../db");
+const shortid = require("shortid");
+const date = require("date-and-time");
+const bcrypt = require("bcrypt");
 
-const cloudinary = require('cloudinary');
-require('../handlers/mongo');
-const Users = require('../models/users/users.model');
+const cloudinary = require("cloudinary");
+require("../handlers/mongo");
+const Users = require("../models/users/users.model");
 
 module.exports.index = (req, res) => {
-	Users.find((err, data) => {
-		if (err) throw err;
-		res.render('users/users', {
-			users: data,
-		});
-	});
+  Users.find((err, data) => {
+    if (err) throw err;
+    res.render("users/users", {
+      users: data,
+    });
+  });
 };
 
 module.exports.search = (req, res) => {
-	const q = req.query.q;
-	const matchedUsers = db
-		.get('users')
-		.value()
-		.filter((user) => {
-			return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-		});
+  const q = req.query.q;
+  const matchedUsers = db
+    .get("users")
+    .value()
+    .filter((user) => {
+      return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
 
-	res.render('users/users', {
-		users: matchedUsers,
-		values: q,
-	});
+  res.render("users/users", {
+    users: matchedUsers,
+    values: q,
+  });
 };
 
 module.exports.view = async (req, res) => {
-	await Users.findById(req.params.id, (err, data) => {
-		if (err) {
-			res.json({ Mgs: err });
-		} else {
-			res.render('users/view', { user: data });
-		}
-	});
+  await Users.findById(req.params.id, (err, data) => {
+    if (err) {
+      res.json({ Mgs: err });
+    } else {
+      res.render("users/view", { user: data });
+    }
+  });
 };
 
 module.exports.create = (req, res) => {
-	res.render('users/create');
+  res.render("users/create");
 };
 
 module.exports.postCreate = async (req, res) => {
-	const hashedPassword = await bcrypt.hash(req.body.password, 10);
-	const sessionId = shortid.generate();
-	const day = date.format(new Date(), 'DD-MM-YYYY');
-	const fileName = day + '_' + sessionId;
-	const avatar = await cloudinary.v2.uploader.upload(req.file.path, {
-		public_id: `Avatar/${fileName}`,
-		tags: `Avatar`,
-	});
+  await Users.findOne({ username: req.body.username }, (err, user) => {
+    if (user) {
+      res.render("users/create", {
+        errors: ["Username has been used"],
+        values: req.body,
+      });
+      return;
+    }
+  });
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const sessionId = shortid.generate();
+  const day = date.format(new Date(), "DD-MM-YYYY");
+  const fileName = day + "_" + sessionId;
+  let path = req.file.path;
+  const avatar = await cloudinary.v2.uploader.upload(path, {
+    public_id: `Avatar/${fileName}`,
+    tags: `Avatar`,
+  });
 
-	const user = new Users();
-	user.fullname = req.body.fullname;
-	user.username = req.body.username;
-	user.password = hashedPassword;
-	user.avatar = avatar.url;
-	user.save();
-	res.redirect('/users');
+  const user = new Users();
+  user.fullname = req.body.fullname;
+  user.username = req.body.username;
+  user.password = hashedPassword;
+  user.avatar = avatar.url;
+  user.save();
+  res.redirect("/users");
 };
--- validate/user.validate.js
+++ validate/user.validate.js
@@ -1,20 +1,20 @@
 module.exports.postCreate = (req, res, next) => {
-	const errors = [];
-	if (!req.body.fullname) {
-		errors.push('Name is required.');
-	}
-	if (!req.body.username) {
-		errors.push('Phone is required.');
-	}
-	if (!req.body.password) {
-		errors.push('Name is required.');
-	}
-	if (errors.length) {
-		res.render('users/create', {
-			errors: errors,
-			values: req.body,
-		});
-		return;
-	}
-	next();
+  const errors = [];
+  if (!req.body.fullname) {
+    errors.push("Name is required.");
+  }
+  if (!req.body.username) {
+    errors.push("Username is required.");
+  }
+  if (!req.body.password) {
+    errors.push("Password is required.");
+  }
+  if (errors.length) {
+    res.render("users/create", {
+      errors: errors,
+      values: req.body,
+    });
+    return;
+  }
+  next();
 };
